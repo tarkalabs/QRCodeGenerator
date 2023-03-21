2231b58eb61e8e325645574e3e652477bb86fef3
Ibrahim Hassan <mdibrahimhassan@gmail.com>
2023-03-21 16:58:16 +0530

    Initial Commit

---
+++ QRCode-Generator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  QRCode-Generator
+//
+//  Created by Ibrahim Hassan on 21/03/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
