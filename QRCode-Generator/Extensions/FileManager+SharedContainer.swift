2504f1c97dcc8ced21cbd08a8902d6e4d7fa8cf7
Ibrahim Hassan <mdibrahimhassan@gmail.com>
2023-06-26 11:06:40 +0530

    Updated bundle id and app group

--- QRCode-Generator/Extensions/FileManager+SharedContainer.swift
+++ QRCode-Generator/Extensions/FileManager+SharedContainer.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension FileManager {
-    private static let appGroupName = "546C62BX57.com.tarka.QR-Genie"
+    private static let appGroupName = "546C62BX57.com.tarka.QRtist"
 
     static var sharedContainerURL: URL? {
         FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroupName)
